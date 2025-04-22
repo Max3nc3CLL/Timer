33ed828b0c59ac71d133d20c72ef75d47a724b65
Max3nc3CLL <maxence.cailleau1@gmail.com>
2025-04-22 16:07:21 +0200

    Initial Commit

---
+++ SimpleTimerMac/SimpleTimerMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTimerMacApp.swift
+//  SimpleTimerMac
+//
+//  Created by Maxence Cailleau on 22/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTimerMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
